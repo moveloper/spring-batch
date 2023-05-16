0f88c6ec8ecddcf1f3ed127b92512b299e8219b7
Soon-Kwon <koentc010@gmail.com>
2023-05-16 22:51:28 +0900

    Initial commit

---
+++ src/test/java/io/springbatch/springbatchlecture/SpringBatchApplicationTests.java
@@ -0,0 +1,15 @@
+package io.springbatch.springbatchlecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@EnableBatchProcessing
+class SpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
